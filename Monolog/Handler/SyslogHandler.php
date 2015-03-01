cbd37b4219457576c086eec942efe402c5c9906c
Benjamin Doherty <bjd@bangpound.org>
2015-03-01 09:20:31 -0600

    initial commit.

---
+++ Monolog/Handler/SyslogHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Caxy\AppEngine\Monolog\Handler;
+
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+
+/**
+ * Class SyslogHandler
+ * @package Caxy\AppEngine\Monolog\Handler
+ */
+class SyslogHandler extends AbstractProcessingHandler
+{
+    /**
+     * Translates Monolog log levels to syslog log priorities.
+     */
+    protected $logLevels = array(
+      Logger::DEBUG     => LOG_DEBUG,
+      Logger::INFO      => LOG_INFO,
+      Logger::NOTICE    => LOG_NOTICE,
+      Logger::WARNING   => LOG_WARNING,
+      Logger::ERROR     => LOG_ERR,
+      Logger::CRITICAL  => LOG_CRIT,
+      Logger::ALERT     => LOG_ALERT,
+      Logger::EMERGENCY => LOG_EMERG,
+    );
+
+    /**
+     * Writes the record down to the log of the implementing handler
+     *
+     * @param  array $record
+     * @return void
+     */
+    protected function write(array $record)
+    {
+        syslog($this->logLevels[$record['level']], (string) $record['formatted']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDefaultFormatter()
+    {
+        return new LineFormatter('%channel%: %message% %context% %extra%');
+    }
+}
---
+++ Pimple/Provider/AppEngineProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Caxy\AppEngine\Pimple\Provider;
+
+use Caxy\AppEngine\Twig\Environment;
+use google\appengine\api\cloud_storage\CloudStorageTools;
+use Caxy\AppEngine\Monolog\Handler\SyslogHandler;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Silex\Provider\MonologServiceProvider;
+
+/**
+ * Class GoogleAppEngineProvider
+ */
+class AppEngineProvider implements ServiceProviderInterface
+{
+    /**
+     *
+     */
+    const GAE_APP_ID = 'Google App Engine';
+
+    private $isAppEngine = false;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->isAppEngine = substr($_SERVER['SERVER_SOFTWARE'], 0, strlen(self::GAE_APP_ID)) === self::GAE_APP_ID;
+    }
+
+    /**
+     * {@inheritdocs}
+     */
+    public function register(Container $pimple)
+    {
+        if (!$this->isAppEngine) {
+            return;
+        }
+
+        $pimple['google.storage_bucket.default'] = CloudStorageTools::getDefaultGoogleStorageBucketName();
+
+        $pimple['twig.environment_factory'] = $pimple->protect(function (Container $pimple) {
+            $options = array('cache' => 'gs://'.$pimple['google.storage_bucket.default'].'/var/cache/twig');
+
+            return new Environment($pimple['twig.loader'], array_merge($pimple['twig.options'], $options));
+        });
+
+        $pimple['monolog.handler'] = function (Container $pimple) {
+            $level = MonologServiceProvider::translateLevel($pimple['monolog.level']);
+
+            $handler = new SyslogHandler($level, $pimple['monolog.bubble']);
+
+            return $handler;
+        };
+    }
+}
