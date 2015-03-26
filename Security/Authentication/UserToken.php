2a170e1d4054259304775e11e215aae181f7937c
Benjamin Doherty <bjd@bangpound.org>
2015-03-26 11:18:58 -0500

    don't add user role to GAE users automatically.

--- Security/Authentication/UserToken.php
+++ Security/Authentication/UserToken.php
@@ -2,22 +2,18 @@
 
 namespace Caxy\AppEngine\Bridge\Security\Authentication;
 
+use google\appengine\api\users\User;
 use google\appengine\api\users\UserService;
 use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
 
 class UserToken extends AbstractToken
 {
-    public function __construct()
+    public function __construct($roles = array())
     {
-        $roles = [];
-        $user = UserService::getCurrentUser();
-        if ($user) {
-            $roles[] = 'ROLE_USER';
+        if (UserService::isCurrentUserAdmin() && !in_array('ROLE_SUPER_ADMIN', $roles)) {
+            $roles[] = 'ROLE_SUPER_ADMIN';
         }
-        if (UserService::isCurrentUserAdmin()) {
-            $roles[] = 'ROLE_ADMIN';
-        }
-        $this->setUser($user);
+        $this->setUser(UserService::getCurrentUser());
         $this->setAuthenticated(count($roles) > 0);
         parent::__construct($roles);
     }
